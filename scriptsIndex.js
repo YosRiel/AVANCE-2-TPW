8a12ef4caabd50e1d2a397a2845c172d43bb8b2f
yosriel <diego.falla.2004.10@gmail.com>
2024-11-22 00:28:46 -0500

    22/11/24 - Diego 00:28

--- scriptsIndex.js
+++ scriptsIndex.js
@@ -1,3 +1,18 @@
-function addToCart() {
-    alert("Producto añadido al carrito.");
-}
+document.addEventListener("DOMContentLoaded", () => {
+    const dropdown = document.querySelector(".dropdown");
+    const dropdownContent = document.querySelector(".dropdown-content");
+    const mainContent = document.querySelector(".main");
+  
+    dropdown.addEventListener("mouseenter", () => {
+      dropdownContent.style.display = "block";
+      mainContent.style.transition = "margin-top 0.3s ease";
+      mainContent.style.marginTop = `${dropdownContent.offsetHeight}px`;
+    });
+  
+    dropdown.addEventListener("mouseleave", () => {
+      dropdownContent.style.display = "none";
+      mainContent.style.transition = "margin-top 0.3s ease";
+      mainContent.style.marginTop = "0";
+    });
+  });
+  
